1647e7475a8aac5fcf4ff1d3f64d0323e5683393
MaxBerezhnoi <mohunt.leo@gmail.com>
2022-09-27 23:53:59 +0300

    part-3_filter(разметка)

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,5 +1,6 @@
 
 import ContactsEl from "components/ContactsEl";
+import SearchField from "components/SearchField";
 import css from "./Contacts.module.css";
 
 export default function Contacts({ contacts, id }) {
@@ -7,6 +8,7 @@ export default function Contacts({ contacts, id }) {
     return (
         <div className={css.contacts}>
             <h1>Contacts</h1>
+            <SearchField />
             <ul>
                 {contacts.map(contact => (<ContactsEl item={contact.name} itemNumber={ contact.number } id ={{id}} />))} 
             </ul>
---
+++ src/components/SearchField/index.jsx
@@ -0,0 +1,11 @@
+export default function SearchField({ }) {
+    return (
+        <div>
+            <p>
+                Find contacts by name:
+            </p>
+            <input type="text" name="searchField" value="" onCheck="" id=""/>
+            
+        </div >
+    )
+}
